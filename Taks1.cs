941c29b2dc6054df8ceef007ba544f805834a82c
semptrastar <alexeycacheknew@gmail.com>
2017-11-14 01:03:23 +0200

    Base

---
+++ Taks1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AsyncTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"C:\TestAsyncDir");
+            File.WriteAllText(@"C:\TestAsyncDir\test.txt", "Async test file, read this");
+
+            DemoAsync();
+
+            File.Delete(@"C:\TestAsyncDir\test.txt");
+            Directory.Delete(@"C:\TestAsyncDir");
+
+            Console.WriteLine("Main ends.");
+        }
+
+        static async Task DemoAsync()
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            try
+            {
+                string text = await ReadAllTextAsync(@"C:\TestAsyncDir\test.txt", tokenSource.Token);
+                Console.WriteLine(text);
+
+                text = await ReadAllTextTrueAsync(@"C:\TestAsyncDir\test.txt", tokenSource.Token);
+                Console.WriteLine(text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                throw;
+            }
+        } 
+
+        static async Task<string> ReadAllTextAsync(string path, CancellationToken cancellationToken)
+        {
+            return await File.ReadAllTextAsync(path, cancellationToken);
+        }
+
+        static async Task<string> ReadAllTextTrueAsync(string path, CancellationToken cancellationToken)
+        {
+            return await Task.Run(() => File.ReadAllText(path), cancellationToken);
+        }
+    }
+}
---
+++ Task3.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeNumberCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            BigInteger numberOfPrimesAsync = 0;
+
+            sw.Start();
+
+            // Async (for 1000000)
+            // Result = 78568
+            // Time = 2,817
+            
+            CalculateNumberOfPrimesAsync(1000000, numberOfPrimesAsync, current => numberOfPrimesAsync += current);
+
+            sw.Stop();
+
+            Console.WriteLine("Async.");
+            Console.WriteLine("Result = " + numberOfPrimesAsync);
+            Console.WriteLine("Time = " + (double)sw.ElapsedMilliseconds / 1000);
+
+
+            sw.Reset();
+            sw.Start();
+
+            // Parallel (for 1000000)
+            // Result = 78568
+            // Time = 2,54
+
+            var numberOfPrimesParallel = CalculateNumberOfPrimesParallel(1000000);
+
+            sw.Stop();
+
+            Console.WriteLine("Parallel.");
+            Console.WriteLine("Result = " + numberOfPrimesParallel);
+            Console.WriteLine("Time = " + (double)sw.ElapsedMilliseconds / 1000);
+
+        }
+
+        // Async
+
+        static async void CalculateNumberOfPrimesAsync(BigInteger bound, BigInteger numberOfPrimes, Action<BigInteger> addNumberOfPrimes)
+        {
+            var calculationTasks = new List<Task<BigInteger>>();
+            BigInteger part = bound / 8;
+
+            for (int i = 0; i < 8; i++)
+            {
+                calculationTasks.Add(CalculatePartNumberOfPrimes(part * i, part * i + part));
+            }
+
+            foreach (var task in calculationTasks)
+            {
+                addNumberOfPrimes(task.Result);
+            }
+        }
+
+        static async Task<BigInteger> CalculatePartNumberOfPrimes(BigInteger from, BigInteger to)
+        {
+            return await Task.Run(() =>
+            {
+                BigInteger numberOfPrimes = 0;
+
+                for (BigInteger number = from; number < to; number++)
+                {
+                    if (IsPrime(number))
+                    {
+                        numberOfPrimes++;
+                    }
+                }
+
+                return numberOfPrimes;
+            });
+        }
+
+        // Parallel
+
+        static BigInteger CalculateNumberOfPrimesParallel(BigInteger bound)
+        {
+            return Range(0, bound).AsParallel().Sum(number => IsPrime(number) ? 1 : 0);
+        }       
+
+        static IEnumerable<BigInteger> Range(BigInteger min, BigInteger max)
+        {
+            for (BigInteger num = min; num <= max; num++)
+            {
+                yield return num;
+            }
+        }
+           
+
+        static bool IsPrime(BigInteger number)
+        {
+            if (number >= 0 && number <= 2)
+                return true;
+
+            for (int i = 2; i <= Math.Exp(BigInteger.Log(number) / 2); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
